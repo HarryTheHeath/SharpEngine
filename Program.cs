67b5ab16528e9ec8a1cfd1943bd66ea8301b44d8
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-11-09 15:11:56 +0100

    moved code chunks to methods

--- Program.cs
+++ Program.cs
@@ -1,77 +1,83 @@
 ﻿using System;
 using System.IO;
 using GLFW;
-using OpenGL;
 using static OpenGL.Gl;
 
 namespace SharpEngine
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //initialize and configure
-            Glfw.Init();
-            Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
-            Glfw.WindowHint(Hint.ContextVersionMajor, 3);
-            Glfw.WindowHint(Hint.ContextVersionMinor, 3);
-            Glfw.WindowHint(Hint.Decorated, true);
-            Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
-            Glfw.WindowHint(Hint.Doublebuffer, Constants.False);
-            //Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
+        static void Main(string[] args) {
+            var window = CreateWindow();
 
-            // create and launch a window
-            var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
-            Glfw.MakeContextCurrent(window);
-            Import(Glfw.GetProcAddress);
+            LoadTriangleIntoBuffer();
 
-            float[] vertices = new float[]
-            {
-                -0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0f, 0.5f, 0f
-            };
-            
-            // load vertices into a buffer
-            var vertexArray = glGenVertexArray();
-            var vertexBuffer = glGenBuffer();
-            glBindVertexArray(vertexArray);
-            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-            unsafe
-            {
-                fixed (float* vertex = &vertices[0])
-                {
-                    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
-                }
-                
-                glVertexAttribPointer(0 ,3, GL_FLOAT, false, 3 * sizeof(float), NULL);
-            }
-            glEnableVertexAttribArray(0);
-
-            
-
-            var vertexShader = glCreateShader(GL_VERTEX_SHADER);
-            glShaderSource(vertexShader, File.ReadAllText(("shaders/red_triangle.vert")));
-            glCompileShader(vertexShader);
-            
-            var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-            glShaderSource(fragmentShader, File.ReadAllText(("shaders/red_triangle.frag")));
-            glCompileShader(fragmentShader);
-            
-            var program = glCreateProgram();
-            glAttachShader(program, vertexShader);
-            glAttachShader(program, fragmentShader);
-            glLinkProgram(program);
-            glUseProgram(program);
+            CreateShaderProgram();
 
             // engine rendering loop
-            while (!Glfw.WindowShouldClose(window))
-            {
+            while (!Glfw.WindowShouldClose(window)) {
                 Glfw.PollEvents(); // react to window changes (position etc.)
                 glDrawArrays(GL_TRIANGLES, 0, 3);
                 glFlush();
             }
         }
 
+        static void CreateShaderProgram() {
+            // create vertex shader
+            var vertexShader = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertexShader, File.ReadAllText("shaders/red-triangle.vert"));
+            glCompileShader(vertexShader);
+
+            // create fragment shader
+            var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragmentShader, File.ReadAllText("shaders/red-triangle.frag"));
+            glCompileShader(fragmentShader);
+
+            // create shader program - rendering pipeline
+            var program = glCreateProgram();
+            glAttachShader(program, vertexShader);
+            glAttachShader(program, fragmentShader);
+            glLinkProgram(program);
+            glUseProgram(program);
+        }
+
+        static unsafe void LoadTriangleIntoBuffer() {
+            float[] vertices = new float[] {
+                -0.5f, -.05f, 0f,
+                0.5f, -0.5f, 0f,
+                0f, 0.5f, 0f
+            };
+
+            // load the vertices into a buffer
+            var vertexArray = glGenVertexArray();
+            var vertexBuffer = glGenBuffer();
+            glBindVertexArray(vertexArray);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+            fixed (float* vertex = &vertices[0]) {
+                glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
+            }
+
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), NULL);
+
+            glEnableVertexAttribArray(0);
+        }
+
+        static Window CreateWindow() {
+            // initialize and configure
+            Glfw.Init();
+            Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
+            Glfw.WindowHint(Hint.ContextVersionMajor, 3);
+            Glfw.WindowHint(Hint.ContextVersionMinor, 3);
+            Glfw.WindowHint(Hint.Decorated, true);
+            Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
+            Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
+            Glfw.WindowHint(Hint.Doublebuffer, Constants.False);
+
+            // create and launch a window
+            var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
+            Glfw.MakeContextCurrent(window);
+            Import(Glfw.GetProcAddress);
+            return window;
+        }
     }
 }
