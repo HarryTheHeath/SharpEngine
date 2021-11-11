6b440c9d4b330e51b092d1a8a179cd1329926e69
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-11-11 17:05:07 +0100

    Refactored, moved Triangle logic to Triangle.cs

--- Program.cs
+++ Program.cs
@@ -11,99 +11,6 @@ using Monitor = GLFW.Monitor;
 
 namespace SharpEngine
 {
-    public class Triangle
-    {
-        Vertex[] vertices;
-        public float CurrentScale {get; private set;}
-
-
-        public Triangle(Vertex[] vertices)
-        {
-            this.vertices = vertices;
-            CurrentScale = 1f;
-        }
-
-        public void Scale(float multiplier)
-        {
-            // 1. Scale the Triangle without Moving it
-
-            // 1.1 Move the Triangle to the Center, so we can scale it without Side Effects
-            // 1.1.1 Find the Center of the Triangle
-            // 1.1.1.1 Find the Minimum and Maximum
-            var min = vertices[0].position;
-            for (var i = 1; i < vertices.Length; i++)
-            {
-                min = Vector.Min(min, vertices[i].position);
-            }
-
-            var max = vertices[0].position;
-            for (var i = 1; i < vertices.Length; i++)
-            {
-                max = Vector.Max(max, vertices[i].position);
-            }
-
-            // 1.1.1.2 Average out the Minimum and Maximum to get the Center
-            var center = (GetMinBounds() + GetMaxBounds()) / 2;
-            Move(center*-1);
-            for (var i = 0; i < vertices.Length; i++)
-            {
-                vertices[i].position -= center;
-            }
-
-            Move(center);
-
-            // 1.2 Scale the Triangle
-            for (var i = 0; i < vertices.Length; i++)
-            {
-                vertices[i].position *= multiplier;
-            }
-
-            // 1.3 Move the Triangle Back to where it was before
-            for (var i = 0; i < vertices.Length; i++)
-            {
-                vertices[i].position += center;
-            }
-
-            CurrentScale *= multiplier;
-        }
-
-        public void Move(Vector direction)
-        {
-            for (var i = 1; i < vertices.Length; i++)
-            {
-                vertices[i].position += direction;
-            }
-        }
-
-        public Vector GetMinBounds()
-        {
-            var min = vertices[0].position;
-            for (var i = 1; i < vertices.Length; i++)
-            {
-                min = Vector.Min(min, vertices[i].position);
-            }
-
-            return min;
-        }
-
-        public Vector GetMaxBounds()
-        {
-            var max = vertices[0].position;
-            for (var i = 1; i < vertices.Length; i++)
-            {
-                max = Vector.Max(max, vertices[i].position);
-            }
-
-            return max;
-        }
-        
-        public unsafe void Render() {
-            fixed (Vertex* vertex = &vertices[0]) {
-                glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.Length, vertex, GL_DYNAMIC_DRAW);
-            }
-            glDrawArrays(GL_TRIANGLES, 0, vertices.Length);
-        }
-    }
     class Program
     {
         static Triangle triangle = new Triangle(new Vertex[] {
@@ -124,20 +31,16 @@ namespace SharpEngine
             new Vector(-0.1f, -0.3f),
             new Vector(-0.2f, -0.1f)
         };*/
-        
-        const int vertexSize = 3;
-        static bool test;
+       
         
         static void Main(string[] args)
         {
             var window = CreateWindow();
-            var direction = new Vector(0.0003f, 0.0003f);
-            var multiplier = 0.999f;
-            var scale = 1f;
-
             LoadTriangleIntoBuffer();
             CreateShaderProgram();
-
+            var direction = new Vector(0.0003f, 0.0003f);
+            var multiplier = 0.999f;
+            
             // engine rendering loop
             while (!Glfw.WindowShouldClose(window))
             {
@@ -146,7 +49,6 @@ namespace SharpEngine
                 Render(window);
                 
                 triangle.Scale(multiplier);
-                
                 
                 // 2. Keep track of the Scale, so we can reverse it
                 if (triangle.CurrentScale <= 0.5f) {
@@ -180,22 +82,25 @@ namespace SharpEngine
             }
         }*/
         
-        
+        static void Render(Window window) {
+            triangle.Render();
+            Glfw.SwapBuffers(window);
+        }
 
-        private static void ClearScreen() {
-            glClearColor(0.2f, 0f, 0.2f, 1);
+        static void ClearScreen() {
+            glClearColor(.2f, .05f, .2f, 1);
             glClear(GL_COLOR_BUFFER_BIT);
         }
 
         static void CreateShaderProgram() {
             // create vertex shader
             var vertexShader = glCreateShader(GL_VERTEX_SHADER);
-            glShaderSource(vertexShader, File.ReadAllText("Shaders/position-color.vert"));
+            glShaderSource(vertexShader, File.ReadAllText("shaders/position-color.vert"));
             glCompileShader(vertexShader);
 
             // create fragment shader
             var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-            glShaderSource(fragmentShader, File.ReadAllText("Shaders/vertex-color.frag"));
+            glShaderSource(fragmentShader, File.ReadAllText("shaders/vertex-color.frag"));
             glCompileShader(fragmentShader);
 
             // create shader program - rendering pipeline
@@ -207,7 +112,6 @@ namespace SharpEngine
         }
 
         static unsafe void LoadTriangleIntoBuffer() {
-            // load the vertices into a buffer
             var vertexArray = glGenVertexArray();
             var vertexBuffer = glGenBuffer();
             glBindVertexArray(vertexArray);
@@ -216,9 +120,7 @@ namespace SharpEngine
             glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.color)));
             glEnableVertexAttribArray(0);
             glEnableVertexAttribArray(1);
-
         }
-        
 
         static Window CreateWindow() {
             // initialize and configure
@@ -229,7 +131,7 @@ namespace SharpEngine
             Glfw.WindowHint(Hint.Decorated, true);
             Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
             Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
-            Glfw.WindowHint(Hint.Doublebuffer, Constants.False);
+            Glfw.WindowHint(Hint.Doublebuffer, Constants.True);
 
             // create and launch a window
             var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
