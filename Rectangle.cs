a206853121124e54137f995a93c355234b32e910
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-11-12 17:43:35 +0100

    Class Rectangle  -> rectangle.cs

---
+++ Rectangle.cs
@@ -0,0 +1,15 @@
+﻿namespace SharpEngine
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(float width, float height, Vector position, Color[] color) : base(new Vertex[6])
+        {
+            vertices[0] = new Vertex(new Vector(position.x - width / 2, position.y - height / 2), color[0]);
+            vertices[1] = new Vertex(new Vector(position.x + width / 2, position.y - height / 2), color[1]);
+            vertices[2] = new Vertex(new Vector(position.x - width / 2, position.y + height / 2), color[2]);
+            vertices[3] = new Vertex(new Vector(position.x + width / 2, position.y + height/ 2), color[3]);
+            vertices[4] = new Vertex(new Vector(position.x - width / 2, position.y + height/ 2), color[4]);
+            vertices[5] = new Vertex(new Vector(position.x + width / 2, position.y - height/ 2), color[5]);
+        }
+    }
+}
