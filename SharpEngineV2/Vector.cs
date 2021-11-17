b120cac41bf91907394eaa3d9b3b8b022b32bb44
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-11-17 22:34:42 +0100

    Clean up the Circle Collision Resolving and adds more Circles

--- SharpEngineV2/Vector.cs
+++ SharpEngineV2/Vector.cs
@@ -1,19 +1,17 @@
 using System;
+using System.Numerics;
 
 namespace SharpEngine {
 	public struct Vector {
 		public float x, y, z;
-		
+
 		public static Vector Forward => new Vector(0, 1);
-		public static Vector Backward => new (0, -1);
+		public static Vector Backward => new Vector(0, -1);
+		public static Vector Down => new Vector(0, -1);
 		public static Vector Left => new Vector(-1, 0);
 		public static Vector Right => new Vector(1, 0);
-		
 		public static Vector Zero => new Vector(0, 0);
-		public static float Dot(Vector a, Vector b)
-		{ return a.x * b.x + a.y * b.y + a.z * b.z; }
 
-		
 		public Vector(float x, float y, float z) {
 			this.x = x;
 			this.y = y;
@@ -28,6 +26,10 @@ namespace SharpEngine {
 
 		public static Vector operator *(Vector v, float f) {
 			return new Vector(v.x * f, v.y * f, v.z * f);
+		}
+
+		public static Vector operator *(float f, Vector v) {
+			return new Vector(f * v.x, f * v.y, f * v.z);
 		}
 
 		public static Vector operator /(Vector v, float f) {
@@ -53,19 +55,30 @@ namespace SharpEngine {
 			return new Vector(MathF.Min(a.x, b.x), MathF.Min(a.y, b.y), MathF.Min(a.z, b.z));
 		}
 
+		public static float Dot(Vector a, Vector b) {
+			return a.x * b.x + a.y * b.y + a.z * b.z;
+		}
+
 		public static float Angle(Vector v) {
 			return MathF.Atan2(v.y, v.x);
 		}
 
 		public float GetMagnitude() {
-			return MathF.Sqrt(x * x + y * y + z * z);
+			return MathF.Sqrt(GetSquareMagnitude());
 		}
 
-		public Vector Normalize()
-		{
+		public float GetSquareMagnitude() {
+			return x * x + y * y + z * z;
+		}
+
+		public Vector Normalize() {
 			var magnitude = GetMagnitude();
-			// Don't divide by zero
+			// DO NOT DIVIDE BY ZERO!
 			return magnitude > 0 ? this / GetMagnitude() : this;
+		}
+
+		public override string ToString() {
+			return $"Vector(X:{this.x}, Y:{this.y}, Z:{this.z}";
 		}
 	}
 }
